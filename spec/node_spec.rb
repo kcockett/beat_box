3ea0dc2ff589bb59252e5a3e5ae09b499ec1cc0f
Kaina Cockett <kainacockett@gmail.com>
2023-05-11 17:20:26 -0600

    Update Node class, three tests passed

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,32 +1,36 @@
-require '../lib/node'
+require './lib/node.rb'
+require 'rspec'
 
-RSpec.describe Node do
-  it 'exists' do
-    node = Node.new("boom")
+describe Node do
+    describe '#initialize' do
+        it 'exists' do
+            node = Node.new("boom")
+        
+            expect(node).to be_instance_of(Node)
+          end
+        
+          it 'has accepts data' do
+            node = Node.new("boom")
+        
+            expect(node.data).to eq("boom")
+          end
+        
+          it 'has no next_node' do
+            node = Node.new("boom")
+        
+            expect(node.next_node).to eq(nil)
+          end
+        
+          it 'accepts only one data element' do
+            node = Node.new("boom tit boom")
+        
+            expect(node.data).to eq("boom")
+          end
+          it 'accepts only string data' do
+            node = Node.new(123)
+        
+            expect(node.data.class).to eq(String)
+          end
 
-    expect(node).to be_instance_of(Node)
-  end
-
-  it 'has accepts data' do
-    node = Node.new("boom")
-
-    expect(node.data).to eq("boom")
-  end
-
-  it 'has no next_node' do
-    node = Node.new("boom")
-
-    expect(node.next_node).to eq(nil)
-  end
-
-  it 'accepts only one data element' do
-    node = Node.new("boom tit boom")
-
-    expect(node.data).to eq("boom")
-  end
-  it 'accepts only string data' do
-    node = Node.new(123)
-
-    expect(node.data.class).to eq(String)
-  end
+    end
 end

cec962bd402167102d69ab63be666f9e618e76f4
Kaina Cockett <kainacockett@gmail.com>
2023-05-15 19:11:03 -0600

    feature: Add beat_box method

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -4,6 +4,6 @@ require "./lib/linked_list"
 class BeatBox
     attr_reader :head
     def initialize
-        #
+        @list = LinkedList.new
     end
 end
--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -2,11 +2,13 @@ require "./lib/linked_list"
 require "./lib/node"
 require "rspec"
 
-describe LinkedList do
-    it 'exists' do
-        bb = BeatBox.new
-        expect(bb).to be_instance_of(BeatBox)
-        expect(bb.list).to be_instance_of(LinkedList)
-        expect(bb.list.head_.to eq nil
+describe BeatBox do
+    describe "#initialize" do
+        it 'exists with default configuration' do
+            bb = BeatBox.new
+            expect(bb).to be_instance_of(BeatBox)
+            expect(bb.list).to be_instance_of(LinkedList)
+            expect(bb.list.head).to eq nil
+        end
     end
 end
